0cec420ffc42200b0afa8cf4c99088904936d2f9
overlord <rexatuzie@gmail.com>
2023-06-08 13:30:40 +0100

    cfghj,

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,15 +10,15 @@ import p1 from "../assets/p1.jpg";
 const Portfolio = () => {
   // ZiltChat is  chatting website, where you can connect with people from all over the world! Our site provides a safe and friendly environment for you to chat with others, whether you're looking for new friends, a casual conversation, or a meaningful connection.You can create your own profile, upload photos, and start chatting instantly with our private messaging system.
   const portfolio = [
-    {
-      id: 1,
-      name: "Chat App",
-      img: chat,
-      desc: "ZiltChat is  chatting app,  friendly environment for you to chat with others, whether you're looking for new friends, a casual conversation, or a meaningful connection.You can create your own profile, upload photos, and start chatting instantly with our private messaging system. ",
-      stack: ["Javascript", "React Js ", "Tailwind CSS", "Google Firebase"],
-      github: "https://github.com/mr-atuzie/chatting_app",
-      link: "https://main--golden-boba-1b00ba.netlify.app/",
-    },
+    // {
+    //   id: 1,
+    //   name: "Chat App",
+    //   img: chat,
+    //   desc: "ZiltChat is  chatting app,  friendly environment for you to chat with others, whether you're looking for new friends, a casual conversation, or a meaningful connection.You can create your own profile, upload photos, and start chatting instantly with our private messaging system. ",
+    //   stack: ["Javascript", "React Js ", "Tailwind CSS", "Google Firebase"],
+    //   github: "https://github.com/mr-atuzie/chatting_app",
+    //   link: "https://main--golden-boba-1b00ba.netlify.app/",
+    // },
     {
       id: 2,
       name: "E-commerce App",
